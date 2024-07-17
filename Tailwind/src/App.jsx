f4f801bfd1a58bbac40face689c7a0d45bea77b3
abhi23zc <you@example.com>
2024-07-17 13:59:52 +0530

    Tailwind CSS and Props

---
+++ Tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-green-400 text-black  rounded-xl p-4 m-auto">
+        Tailwind{" "}
+        
+      </h2>
+
+      <Card title="Macbook" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, delectus."  />
+
+    </>
+  );
+}
+
+export default App;
